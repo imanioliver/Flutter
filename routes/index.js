372839c0f2093d3fcbc4bea6d644fa737c9e5a4d
Imani Oliver <isaac@ihardy.co>
2017-09-04 12:16:54 -0400

    delete functionality works, except for all users

--- routes/index.js
+++ routes/index.js
@@ -23,15 +23,34 @@ router.get("/", isAuthenticated, function(req, res) {
             {
                 model: User,
                 as: "user"
+            },
+            {
+                model:Like,
+                as: "likes"
             }
         ]
     })
-    .then(function(messages) {
-        console.log(messages);
-        res.render("home", {messages: res.locals.getMessages(), allMessages:messages});
+    .then(function(data) {
+        // console.log(data);
+        let currentUser = req.user.id;
+
+        res.render("home", {messages: res.locals.getMessages(), allMessages:data
+
+            // , messageID: message.dataValues.id
+            // ,messageCount:
+        }
+        );
     })
+    // .then(function(likeCount){
+    //     Likes.findAll({
+    //         where: {
+    //             Message
+    //         }
+    //     })
+    // })
     .catch(function(err) {
         console.log(err);
+
         res.redirect("/")
     });
 });
@@ -82,22 +101,97 @@ router.get("/logout", function(req, res) {
 });
 
 router.get("/create", isAuthenticated, function(req, res){
-    res.render("create");
+
+    Message.findAll({
+        include: [
+            {
+                model: User,
+                as: "user"
+            },
+            {
+                model:Like,
+                as: "likes"
+            }
+        ]
+    })
+
+    .then(function(data){
+        res.render("create", {allMessages:data})
+    })
 });
 
 router.post("/create", function(req, res){
     // console.log(req.user);
 
     Message.create({
-            title: req.body.title,
             text: req.body.text,
             userId: req.user.id
         })
     .then(function(data){
-        // console.log(data);
+        console.log(data);
       res.redirect("/");
     });
 });
+
+router.post("/like/:id", function(req, res){
+
+    let id= req.params.id;
+    // console.log(id);
+    // console.log(req.user.id);
+    Like.create({
+        userId: req.user.id,
+        messageId: id
+    })
+    .then(function(data){
+        // console.log(data);
+        res.redirect("/");
+    })
+
+
+    //you should do a req.params.id to send to the like page
+})
+
+// router.post("/viewOne/:id", function(req, res){
+//     let id= req.params.id;
+//
+//     Message.findById(req.params.id)
+//
+//     .then(function(data){
+//
+//         res.render("oneMessage");
+//
+//     })
+//
+// };
+
+router.post("/viewOne/:id", function(req, res){
+    let id= req.params.id;
+
+    Message.findById(req.params.id)
+    .then(function(data){
+
+        res.render("oneMessage");
+
+    })
+});
+
+
+router.get("/delete/:id", function (req, res){
+    Like.destroy({
+        where: {
+            messageId: req.params.id
+        }
+    })
+    .then(function(data){
+        Message.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        console.log(data);
+        res.redirect("/")
+    });
+})
 
 
 module.exports = router;
